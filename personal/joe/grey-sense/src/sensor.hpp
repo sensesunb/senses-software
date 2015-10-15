323244e34793f6c634c47fae867b7b341737760a
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-10-15 15:28:58 -0300

    Began creating the identifier object to change some approaches

--- personal/joe/grey-sense/src/sensor.hpp
+++ personal/joe/grey-sense/src/sensor.hpp
@@ -8,6 +8,7 @@
 #define SIMILARITY (0.9)
 
 class Sensor {
+	float *identify_portion(Letter);
 	std::string typeface;
 	Comparator comparator;
 public:
@@ -18,7 +19,7 @@ public:
 	Letter get_letter(char);
 	/* std::string sense(char*) */ // this function will extract the fucking text
 	std::vector<int*> find(BITMAP*, Letter);
-
+	std::vector<float*> identify(BITMAP*);
 };
 
 /************************
@@ -122,6 +123,65 @@ std::vector<int*> Sensor::find(BITMAP* image, Letter letter)
 			// printf("\n");
 			portion.clean();
 		}
+	}
+
+	return found;
+}
+
+float* Sensor::identify_portion(Letter portion)
+{
+	std::vector<Letter> templates = comparator.get_templates();
+	Letter letter;
+	float max_result = 0;
+	float result = 0;
+	int index = 0;
+	float *found = NULL;
+
+	for (int i = 0; i < templates.size(); ++i)
+	{
+		letter = templates.at(i);
+		result = letter.compare(portion);
+		if (max_result < result) {
+			index = i;
+			max_result = result;
+		}
+	}
+
+	if (max_result > SIMILARITY) {
+		found = (float*) malloc(5 * sizeof(float));
+		found[0] = '0' + index;
+		found[1] = 100 * result;
+	}
+
+	return found;
+}
+
+std::vector<float*> Sensor::identify(BITMAP *image)
+{
+	Cropper cropper;
+	Letter portion;
+	std::vector<float*> found;
+	float *result;
+	int bx = 0;
+	int by = 0;
+	int ex = image->width - 32;
+	int ey = image->height - 32;
+	int sx = 1;
+	int sy = 1;
+	int h = 32+1;
+
+	for (int y = by; y < ey; y += sy)
+	{
+		for (int x = bx; x < ex; x += sx)
+		{
+			portion.set_pattern(cropper.crop(image, x, y, x+h, y+h));
+			result = identify_portion(portion);
+			if (result != NULL) {
+				result[2] = x;
+				result[3] = y;
+				found.push_back(result);
+			}
+			portion.clean();
 	}
 
 	return found;
---
+++ personal/joe/many/identifier.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <bitmap.h>
+#include <sensor.hpp>
+
+void show_results(std::vector<float*> found)
+{
+	for (int i = 0; i < found.size(); ++i)
+	{
+		std::cout << "- " << (char) found[i][0] << std::endl;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	Sensor input_font;
+	Sensor processing_font;
+	std::vector<float*> found;
+	BITMAP *bitmap;
+
+	input_font.load("templates/Input");
+	processing_font.load("templates/Processing");
+	bitmap = bitmap_load(argv[1]);
+
+	std::cout << "--- # Input" << std::endl;
+	found = input_font.identify(bitmap);
+	if (found.length() > 0)
+		show_results(found);
+	std::cout << std::endl;
+
+	std::cout << "--- # Processing" << std::endl;
+	found = processing_font.identify(bitmap);
+	if (found.length() > 0)
+		show_results(found);
+
+	std::cout << "..." << std::endl;
+
+	return 0;
+}
