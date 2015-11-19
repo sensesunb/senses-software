7fb66f13cecde1a3f262f6783d78a9c21d166d48
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-11-19 15:28:08 -0200

    Arduino can now write Braille. Just some details left to fix...

---
+++ personal/joe/hard/letter2binbraille/letter2binbraille.ino
@@ -0,0 +1,33 @@
+#include "bit2letter.h"
+#include "braille.h"
+
+void setup()
+{
+	Serial.begin(9600);
+}
+
+char get_letter()
+{
+	char junk;
+	char data;
+
+	while (!Serial.available())
+		;
+	data = Serial.read();
+	while (Serial.available())
+		junk = Serial.read();
+
+	return data;
+}
+
+void write_braille(byte b)
+{
+	for (byte i = 0; i < 6; ++i)
+		Serial.print(((b >> i) & 0x1)? '*' : '.');
+	Serial.println();
+}
+
+void loop()
+{
+	write_braille(letter2code(get_letter()));
+}
