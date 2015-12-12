6d16e5a1a6454f0fb9b8bf1745a8dd0fbc8ae9e1
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-12-12 17:09:58 -0200

    bit2letter.h is ready to be used

--- personal/joe/hard/braille-crs94/bit2letter.rb
+++ personal/joe/hard/braille-crs94/bit2letter.rb
@@ -14,6 +14,7 @@ module Bit2Letter
 #{crunchbang}ifndef byte
 #{crunchbang}define byte unsigned char
 #{crunchbang}endif
+
         header
     end
 
@@ -33,6 +34,24 @@ byte letter2code(char letter)
     default: return '?';
   }
 }
+
+        header
+    end
+
+    def Bit2Letter::open_precedence
+        <<-header
+byte get_precedence(char letter)
+{
+  switch (letter)
+  {
+        header
+    end
+
+    def Bit2Letter::close_precedence
+        <<-header
+    default: return 0;
+  }
+}
         header
     end
 
@@ -48,23 +67,40 @@ byte letter2code(char letter)
 
     def Bit2Letter::draw outlet, info
         outlet.puts get_header
+
+        # letter2code function
         outlet.puts open_letter2code
         for key, value in info
             outlet.puts "    case '#{key}': return #{value['code']}; break;"
-            # puts "#{key}: '#{value}'"
         end
         outlet.puts close_letter2code
+
+        # precedence function
+        outlet.puts open_precedence
+        for key, value in info
+            precedence = value['precedence']
+            if precedence > 0
+                outlet.puts "    case '#{key}': return #{precedence}; break;"
+            end
+        end
+        outlet.puts close_precedence
     end
 
     def Bit2Letter::extract_info line, info
         bits = line.split /\s+/
+
+        for bit in bits
+            print bit + ' '
+        end
+        puts
+
         info[bits[0]] = { 'precedence' => get_precedence(bits),
                           'code' => get_code(bits) } if bits[0].length > 0
         info
     end
 
     def Bit2Letter::get_precedence bits
-        precedence = -1
+        precedence = 0
 
         if bits[9] != 'x'
             precedence = 0
@@ -80,7 +116,7 @@ byte letter2code(char letter)
         code = 0
 
         for i in 0...6
-            code += 2**i if bits[16+i].to_i != 0
+            code += 2**i if bits[15+i].to_i != 0
         end
 
         return code
