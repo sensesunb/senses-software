122b4cd474d3203896b3866aeb248f0d01e5e00b
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-12-12 16:56:55 -0200

    Building bit2letter.h wrongly

--- personal/joe/hard/braille-crs94/bit2letter.rb
+++ personal/joe/hard/braille-crs94/bit2letter.rb
@@ -23,7 +23,7 @@ byte letter2code(char letter)
 {
   switch (letter)
   {
-      case ' ': return 0; break;
+    case ' ': return 0; break;
         header
     end
 
@@ -38,15 +38,9 @@ byte letter2code(char letter)
 
     def Bit2Letter::setup inlet
         out = {}
-        no_line = 0
 
         for line in inlet
-            bits = line.split /\s+/
-            if bits[0].length > 0
-                out[no_line] = bits[0]
-            end
-
-            no_line += 1
+            out = extract_info line, out
         end
 
         return out
@@ -56,9 +50,40 @@ byte letter2code(char letter)
         outlet.puts get_header
         outlet.puts open_letter2code
         for key, value in info
-            puts "#{key}: '#{value}'"
+            outlet.puts "    case '#{key}': return #{value['code']}; break;"
+            # puts "#{key}: '#{value}'"
         end
         outlet.puts close_letter2code
+    end
+
+    def Bit2Letter::extract_info line, info
+        bits = line.split /\s+/
+        info[bits[0]] = { 'precedence' => get_precedence(bits),
+                          'code' => get_code(bits) } if bits[0].length > 0
+        info
+    end
+
+    def Bit2Letter::get_precedence bits
+        precedence = -1
+
+        if bits[9] != 'x'
+            precedence = 0
+            for i in 0...6
+                precedence += 2**i if bits[9+i].to_i != 0
+            end
+        end
+
+        return precedence
+    end
+
+    def Bit2Letter::get_code bits
+        code = 0
+
+        for i in 0...6
+            code += 2**i if bits[16+i].to_i != 0
+        end
+
+        return code
     end
 end
 
