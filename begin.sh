de200751fe86284818590be8711fc90d9320891a
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-04-26 13:53:33 -0300

    Updating begin.sh to act according new repo

--- begin.sh
+++ begin.sh
@@ -1,5 +1,5 @@
 git init
 git commit -m "..."
 git remote add
-git remote add origin https://github.com/ishiikurisu/senses.git
+git remote add origin https://github.com/sensesunb/senses-software.git
 gt pull -u origin master
